c323cc80344c0b067ae8af9f76e61e985b9820d7
Senku-qq <maksfloopy@gmail.com>
2024-11-23 22:59:56 +0100

    -

--- src/app/main.py
+++ src/app/main.py
@@ -1,2 +1,21 @@
-if __name__ == 'main':
-    pass
+from bs4 import BeautifulSoup
+import requests
+
+# URL целевой страницы
+class Parser:
+    def parser(url): 
+        # Загружаем содержимое страницы
+        response = requests.get(url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            file = open('text.txt','w', encoding="utf8")
+            text = soup.find_all('p')
+            for title in text:
+                file.write(title.text.strip() + "\n")
+            file.close()
+        else:
+            print(f"Ошибка загрузки страницы: {response.status_code}")
+
+if __name__ == '__main__':
+    tom = Parser
+    tom.parser('https://en.wikipedia.org/wiki/Linux')
