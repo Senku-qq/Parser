eb6fb7b62eb02cd55eaf156eb1c142f026d36cc0
Senku-qq <maksfloopy@gmail.com>
2024-11-30 15:51:33 +0100

    algorithm tests

--- misha_app/misha.py
+++ misha_app/misha.py
@@ -67,7 +67,7 @@ from sumy.utils import get_stop_words
 import nltk
 nltk.download('punkt')
 
-def summarize_paragraph(paragraph, sentences_count=100):
+def summarize_paragraph(paragraph, sentences_count=2):
     parser = PlaintextParser.from_string(paragraph, Tokenizer("ukrainian"))
 
     summarizer = LsaSummarizer(Stemmer("ukrainian"))
@@ -78,7 +78,7 @@ def summarize_paragraph(paragraph, sentences_count=100):
 
 if __name__ == "__main__":
     paragraph = open("misha_app/text.txt", "r", encoding="utf-8").read()
-    sentences_count = 100
+    sentences_count = 5
     summary = summarize_paragraph(paragraph, sentences_count)
 
     with open('text2.txt', 'w', encoding="utf-8") as f:
