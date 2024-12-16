592d444fa6d997388f5cab60e4f0627c5c382fff
shadowww <artemmizh@gmail.com>
2024-12-16 19:38:37 +0200

    added telegram bot configurations

---
+++ app/handlers.py
@@ -0,0 +1,26 @@
+import re
+import logging
+import requests
+
+from bs4 import BeautifulSoup
+from aiogram import F, Router
+from aiogram.filters import CommandStart, Command
+from aiogram import F, Router
+from aiogram.types import Message, MessageEntity
+
+router = Router()
+
+URL_REGEX = r'(https?://[^\s]+|www\.[^\s]+)'
+@router.message(CommandStart())
+async def cmd_start(msg: Message):
+    await msg.answer("Welcome to Our Parser Bot! This bot is used for parsing from any article (link)!")
+
+@router.message(F.text)
+async def parsing(msg: Message):
+    username = msg.from_user.username
+    logging.info(f"Username: @{username} | Message: {msg.text}")
+#   ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+    if re.search(URL_REGEX, msg.text):  
+        await msg.reply("Вы отправили ссылку!")
+    else:
+        await msg.reply(f"Вижу обычный текст: {msg.text}")
--- run.py
+++ run.py
@@ -2,31 +2,16 @@ import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher, F
-from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
+from app.handlers import router
 
 from config import TOKEN
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-@dp.message(F.text)
-async def console(msg: Message):
-    username = msg.from_user.username
-    logging.info(f"Username: @{username} | Message: {msg.text}")
-
-
-@dp.message(CommandStart())
-async def cmd_start(msg: Message):
-    await msg.answer("пиривет")
-
-@dp.message(Command('help'))
-async def cmd_help(msg: Message):
-    await msg.answer('''
-Привіт! Скинь мені будь ласка посилання, і я тобі надішлю найголовне з цього сайту (посилання)
-\n''')
-
 async def main():
+    dp.include_router(router)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
