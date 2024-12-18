6f4644d8f9b730c283564dd4651b61de6e90eb3d
Mikhailo <yegrashich@gmail.com>
2024-12-18 13:59:04 +0000

    keyboard ready. but we need to separate parsing file and summaryzing to create abstracts from files

---
+++ artyom_app/keyboard.py
@@ -0,0 +1,25 @@
+from aiogram.types import InlineKeyboardMarkup , InlineKeyboardButton
+
+settings = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="📝 20%", callback_data="20"),
+            InlineKeyboardButton(text="📝 30%", callback_data="30"),
+            InlineKeyboardButton(text="📝 40%", callback_data="40")
+        ],
+        [
+            InlineKeyboardButton(text="📝 50%", callback_data="50"),
+            InlineKeyboardButton(text="📝 60%", callback_data="60"),
+            InlineKeyboardButton(text="📝 70%", callback_data="70")
+        ],
+        [
+            InlineKeyboardButton(text="🇺🇸 English", callback_data="en"),
+            InlineKeyboardButton(text="🇺🇦 Ukrainian", callback_data="ua"),
+            InlineKeyboardButton(text="🇷🇺 Russian", callback_data="ru"),
+        ],
+        [
+            InlineKeyboardButton(text="📎 Custom", callback_data="custom")
+        ]
+    ],
+    resize_keyboard=True
+)
